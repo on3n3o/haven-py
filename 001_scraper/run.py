604a8de7e7ec7b9f6fa113426f76716f2f21cd65
Marcin Maciejewski <marcin.maciejewski@dataspace.pl>
2018-10-21 13:05:27 +0200

    slight modification

--- 001_scraper/run.py
+++ 001_scraper/run.py
@@ -4,7 +4,6 @@ import re
 visited = []
 emails = []
 
-def 
 data = requests.get('http://ulanelectronics.pl')
 
 links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', data.text)
